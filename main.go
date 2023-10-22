2e049338becd4b9c882f195839f7d678e0fda656
Andries Niemandt <andries.niemandt91@gmail.com>
2023-10-23 00:46:34 +0200

    basic http response and bytes written count

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	println("Hello World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		n, err := fmt.Fprintf(w, "Hello from Golang!")
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+	})
+
+	_ = http.ListenAndServe(":8080", nil)
 }
