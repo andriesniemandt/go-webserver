3e35211bf052bb2aa541d9deb06381dd77eb92c9
Andries Niemandt <andries.niemandt91@gmail.com>
2023-10-26 01:21:23 +0200

    get tailwind to work

--- main.go
+++ main.go
@@ -1,33 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
+	"embed"
 	"net/http"
 )
 
 const PORT = ":8080"
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	RenderTemplate(w, "home.page.html")
-}
-
-func About(w http.ResponseWriter, r *http.Request) {
-	RenderTemplate(w, "about.page.html")
-}
-
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template: ", err)
-		return
-	}
-}
+//go:embed dist
+var dist embed.FS
 
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.Handle("/dist/", http.FileServer(http.FS(dist)))
 
 	_ = http.ListenAndServe(PORT, nil)
 }
